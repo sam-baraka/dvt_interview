8ce502b60512d22923ebcd481b7d7282889cf751
Samuel Baraka <sababuvercetti@gmail.com>
2024-04-14 09:20:44 +0300

    Add color resources

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color sunny = const Color(0xff47ab2f);
+  static Color cloudy = const Color(0xff54717a);
+  static Color rainy = const Color(0xff57575d);
+}
