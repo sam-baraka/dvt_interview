2a1c8751a342d932b8aa2bee4f07b6763968ccb8
Samuel Baraka <sababuvercetti@gmail.com>
2024-04-16 15:59:09 +0300

    Wrap application with a bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:dvt_interview/cubits/current_weather_cubit/current_weather_cubit.dart';
 import 'package:dvt_interview/routes/router.dart';
+import 'package:dvt_interview/services/weather_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: appRouter,
+    return BlocProvider(
+      create: (context) => CurrentWeatherCubit(WeatherService())
+        ..getCurrentWeather(lat: 0.0, lon: 0.0),
+      child: MaterialApp.router(
+        routerConfig: appRouter,
+      ),
     );
   }
 }
