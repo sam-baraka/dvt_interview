ee3965506de39e41c09a38225e1f8a12e07bd36c
Samuel Baraka <sababuvercetti@gmail.com>
2024-04-16 11:30:21 +0300

    Routes configurations

---
+++ lib/routes/router.dart
@@ -0,0 +1,22 @@
+import 'package:dvt_interview/pages/main_home_page.dart';
+import 'package:dvt_interview/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+/// The route configuration.
+final GoRouter appRouter = GoRouter(routes: <RouteBase>[
+  GoRoute(
+    path: '/',
+    builder: (BuildContext context, GoRouterState state) {
+      return const SplashPage();
+    },
+    routes: <RouteBase>[
+      GoRoute(
+        path: 'main-home-page',
+        builder: (BuildContext context, GoRouterState state) {
+          return const MainHomePage();
+        },
+      ),
+    ],
+  ),
+]);
